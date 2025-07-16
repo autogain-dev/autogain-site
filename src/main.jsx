d88b3842b69467f4b84e6cc67652306daf8899b8
autogain-dev <milancharbonneau@hotmail.fr>
2025-07-16 19:25:14 -0400

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Welcome from './welcome'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Welcome />
+  </React.StrictMode>,
+)
