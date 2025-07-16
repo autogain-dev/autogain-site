4d855215ae9e60db915f68a549a753fb1736a07d
autogain-dev <milancharbonneau@hotmail.fr>
2025-07-16 09:16:42 -0400

    Delete autogain-site directory

--- autogain-site/app.js
+++
@@ -1,6 +0,0 @@
-
-function switchLanguage(lang) {
-    document.querySelectorAll('[data-fr]').forEach(el => {
-        el.textContent = lang === 'fr' ? el.getAttribute('data-fr') : el.getAttribute('data-en');
-    });
-}
